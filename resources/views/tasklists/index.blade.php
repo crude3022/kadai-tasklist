3845dcf483e9afb43570f76b173e5d466f160a2e
hiroyuki.sumiuchi <crude0708@gmail.com>
2021-03-12 13:30:28 +0000

    tasklists

---
+++ resources/views/tasklists/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+    <h1>タスク一覧</h1>
+
+    @if (count($tasklists) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasklists as $tasklist)
+                <tr>
+                    <td>{!! link_to_route('task-list.show',$tasklist->id , ['task_list'=> $tasklist->id]) !!}</td>
+                    <td>{{ $tasklist->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+        {{-- メッセージ作成ページへのリンク --}}
+        {!! link_to_route('task-list.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+
+@endsection
---
+++ resources/views/tasklists/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+ <h1>id = {{ $tasklist->id }} のタスク詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasklist->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $tasklist->content }}</td>
+        </tr>
+    </table>
+    
+        {{-- メッセージ編集ページへのリンク --}}
+    {!! link_to_route('task-list.edit', 'このメッセージを編集',['task_list'=> $tasklist->id], ['class' => 'btn btn-light']) !!}
+
+    {{-- メッセージ削除フォーム --}}
+    {!! Form::model($tasklist, ['route' => ['task-list.destroy', $tasklist->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    
+@endsection
